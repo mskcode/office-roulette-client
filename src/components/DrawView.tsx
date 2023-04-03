f498ea38f4eb8d6393b401096825ba3b850632db
Samuli Kolehmainen <samuli.kolehmainen@veikkaus.fi>
2023-04-03 12:43:29 +0300

    Flesh out draw details

--- src/components/DrawView.tsx
+++ src/components/DrawView.tsx
@@ -1,6 +1,6 @@
 import AddIcon from '@mui/icons-material/Add'
-import InfoIcon from '@mui/icons-material/Info'
 import RefreshIcon from '@mui/icons-material/Refresh'
+import RemoveIcon from '@mui/icons-material/Remove'
 import {
   Box,
   Button,
@@ -10,10 +10,13 @@ import {
   TableCell,
   TableContainer,
   TableHead,
-  TableRow
+  TableRow,
+  TextField,
 } from '@mui/material'
+import Grid2 from '@mui/material/Unstable_Grid2'
 import * as React from 'react'
 import { Draw, createDraw, fetchDraws } from '../services/officeRouletteClient'
+
 
 function RefreshDrawTableButton(): JSX.Element {
   const onRefreshDrawTableClick = () => {
@@ -59,7 +62,7 @@ function DrawTable(): JSX.Element {
             >
               <TableCell component="th" scope="row">{draw.id}</TableCell>
               <TableCell align="right">{draw.status}</TableCell>
-              <TableCell align="right"><Button variant="text"><InfoIcon /></Button></TableCell>
+              <TableCell align="right"><Button variant="contained">Select</Button></TableCell>
             </TableRow>
           ))}
         </TableBody>
@@ -80,6 +83,42 @@ function AddDrawButton(): JSX.Element {
   }
   return (
     <Button variant="contained" onClick={onAddDrawClick}><AddIcon />Add Draw</Button>
+  )
+}
+
+function DrawDetailsGrid(): JSX.Element {
+  return (
+    <Box>
+      <Grid2 container spacing={2}>
+        <Grid2 xs={6}>
+          <TextField variant="filled" fullWidth={true} helperText="Draw ID" value={1} />
+        </Grid2>
+        <Grid2 xs={6}>
+          <TextField variant="filled" fullWidth={true} helperText="Status" value={'OPEN'} />
+        </Grid2>
+        <Grid2 xs={4}>
+          <TextField variant="filled" fullWidth={true} helperText="Insert time" value={'1970-01-01T00:00.00.000Z'} />
+        </Grid2>
+        <Grid2 xs={4}>
+          <TextField variant="filled" fullWidth={true} helperText="Draw time" value={'1970-01-01T00:00.00.000Z'} />
+        </Grid2>
+        <Grid2 xs={4}>
+          <TextField variant="filled" fullWidth={true} helperText="Result time" value={'1970-01-01T00:00.00.000Z'} />
+        </Grid2>
+        <Grid2 xs={12}>
+          <TextField variant="filled" fullWidth={true} helperText="Winner" value={'John Smith'} />
+        </Grid2>
+      </Grid2>
+      <Box sx={{ display: 'flex', marginTop: '20px' }}>
+        <Box sx={{ display: 'flex', flexDirection: 'row', justifyContent: 'left', width: '50%' }}>
+          <Button variant="contained"><AddIcon /> Add participants</Button>
+          <Button variant="contained" sx={{ marginLeft: '10px' }}><RemoveIcon />Remove participants</Button>
+        </Box>
+        <Box sx={{ display: 'flex', flexDirection: 'row', justifyContent: 'right', width: '50%' }}>
+          <Button variant="contained">Execute draw</Button>
+        </Box>
+      </Box>
+    </Box>
   )
 }
 
@@ -109,6 +148,11 @@ export default function DrawView(): JSX.Element {
 
       <Box>
         <DrawTable />
+      </Box>
+
+      <Box>
+        <h3>Draw details</h3>
+        <DrawDetailsGrid />
       </Box>
     </Box>
   )
